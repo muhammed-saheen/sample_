b38ab6c044d44ecf8dcbd955330026b3b82a6e88
Muhammed saheen <99274222+muhammed-saheen@users.noreply.github.com>
2025-11-28 10:59:50 +0530

    Change text in paragraph to add greeting
    Updated text in the paragraph to include 'hello welcome'.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test 
+          Edit <code>src/App.jsx</code> hello welcome
         </p>
       </div>
       <p className="read-the-docs">
